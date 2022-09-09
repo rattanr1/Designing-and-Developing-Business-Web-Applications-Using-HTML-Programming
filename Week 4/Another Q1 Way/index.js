b423c8ea96387206f38c3acf459691375f25c444
Riyan R <71060227+rattanr1@users.noreply.github.com>
2022-09-09 06:18:34 -0500

    Add files via upload

---
+++ Week 4/Another Q1 Way/index.js
@@ -0,0 +1,4 @@
+var images=["https://i.picsum.photos/id/237/536/354.jpg?hmac=i0yVXW1ORpyCZpQ-CknuyV-jbtU7_x9EBQVhvT5aRr0","https://dummyimage.com/600x400/000/fff","https://i.picsum.photos/id/237/536/354.jpg?hmac=i0yVXW1ORpyCZpQ-CknuyV-jbtU7_x9EBQVhvT5aRr0","https://dummyimage.com/600x400/000/fff"];
+var description=["Description","Description","Description","Description"];
+var quantity=[3,1,2,4];
+var price=[80,125,75,98]
